08e32bd748797a592bc8bc7724a233351c5f8d4f
SaraFakhet <fakhetsara@gmail.com>
2016-04-20 05:49:17 +0200

    taf de sara

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class PauseMenu : MonoBehaviour
+{
+    public bool isPaused;
+    public GameObject pauseMenuCanvas;
+
+	void Update ()
+    {
+        if (isPaused)
+        {
+            pauseMenuCanvas.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseMenuCanvas.SetActive(false);
+            Time.timeScale = 1;
+        }
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            isPaused = !isPaused;
+        }
+	}
+    public void Resume()
+    {
+        isPaused = false;
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
