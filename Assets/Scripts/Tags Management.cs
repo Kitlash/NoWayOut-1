dd2bb1247d33345002059fe5422ade73451a8d0f
RaseMotte <claire.helme_guizon@epita.fr>
2016-04-21 12:31:35 +0200

    I don't know what I'm pushing

--- Assets/Scripts/Tags Management.cs
+++ Assets/Scripts/Tags Management.cs
@@ -9,4 +9,5 @@ public class Tags : MonoBehaviour
     public const string gameController = "Game Controller";
 	public const string PickUp = "Pick Up";
 	public const string Weapon = "Weapon";
+	public const string WeaponToPickUp = "Weapon to pick up";
 }
