9de4efbfa21337a1c1c4106fdc187627e752da3b
RaseMotte <claire.helme_guizon@epita.fr>
2016-04-25 15:43:50 +0200

    Who commit ith the wrong map so I kill him?

---
+++ Assets/Scripts/OpponentLife.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpponentLife : MonoBehaviour 
+{
+	[SerializeField]
+	float health = 100f;
+
+	float OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.name == "Bullet") 
+		{
+			health -= gameObject.GetComponent<WeaponBase> ().damage;
+		}
+		return health;
+	}
+
+	void Update()
+	{
+		if (health <= 0) 
+		{
+			Destroy (gameObject);
+		}
+	}
+
+}
