3a6b5d2209e7cf739b749ecbdd3ba31d8f93cccd
Leramore <camille.louis@epita.fr>
2016-04-29 18:39:58 +0200

    cam

--- Assets/Scripts/EnemySight.cs
+++ Assets/Scripts/EnemySight.cs
@@ -7,6 +7,7 @@ public class EnemySight : MonoBehaviour
     public bool playerInSight;
     public Vector3 personalLastSighting;
 
+    
     private GameObject player;
     private NavMeshAgent nav;
     private Vector3 previousSighting;
@@ -63,6 +64,7 @@ public class EnemySight : MonoBehaviour
 
                 if(Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, col.radius))
                 {
+                    Debug.Log("youhou");
                     if(hit.collider.gameObject == player)
                     {
                         playerInSight = true;
@@ -70,7 +72,7 @@ public class EnemySight : MonoBehaviour
 
                         //Animation shoot
 
-                        playerHealth.TakeDamage(20);
+                        //playerHealth.TakeDamage(20);
                     }
                 }
             }
