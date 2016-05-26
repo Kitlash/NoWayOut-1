77a64cfbac87a7d9ec37fbe067dc93ee773f2161
RaseMotte <claire.helme_guizon@epita.fr>
2016-05-27 01:50:30 +0200

    b

---
+++ Assets/BossCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossCollider : MonoBehaviour {
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.name == "Player") 
+		{
+			return;
+		}
+	}
+}
