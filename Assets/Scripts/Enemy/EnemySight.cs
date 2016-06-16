03354653b5472a782ff32745232dd834e3a54be0
Leramore <camille.louis@epita.fr>
2016-06-16 15:51:30 +0200

    AI

--- Assets/Scripts/Enemy/EnemySight.cs
+++ Assets/Scripts/Enemy/EnemySight.cs
@@ -14,41 +14,21 @@ public class EnemySight : MonoBehaviour
     private NavMeshAgent nav;
     private Vector3 previousSighting;
     private SphereCollider col;
-    
-    //private Animator anim;
-    
-    //private Animator playerAnim;
 	private PlayerHealth playerHealth;
-    
 
     void Start()
     {
 		nav = GetComponent<NavMeshAgent>();       
-		col = GetComponent<SphereCollider>();
-		//anim = GetComponent<Animator> ();
-		//anim = GameObject.FindGameObjectWithTag(Tags.enemy).GetComponent<Animator>(); // inutile si tu places ton script sur ton prefab ennemy, la ligne au dessus est suffisante je pense 
-        //lastPlayerSighting = GameObject.FindGameObjectWithTag(Tags.gameController).GetComponent<LastPlayerSighting>();        
+		col = GetComponent<SphereCollider>();      
         player = GameObject.FindGameObjectWithTag(Tags.player);
-		//playerAnim = player.GetComponent<Animator>(); // pourquoi t'as besoin de l'animator tu playor?
 		playerHealth = player.GetComponent<PlayerHealth> ();
-        
-        
-		
         personalLastSighting = resetPosition;
     }
 
     void Update()
     {
         if (playerInSight)
-        {
             personalLastSighting = player.transform.position;
-            Debug.Log("personal last sighting");
-        }
-            
-        /*if (position != previousSighting)
-            personalLastSighting = position;
-
-        previousSighting = position;*/
     }
 
     void OnTriggerStay(Collider other)
@@ -56,10 +36,7 @@ public class EnemySight : MonoBehaviour
         if(other.gameObject == player)
         {
             playerInSight = false;
-            
-
             Vector3 direction = other.transform.position - transform.position;
-            //nav.SetDestination(direction);
             float angle = Vector3.Angle(direction, transform.forward);
 
             if(angle < fieldOfViewAngle * 0.5f)
@@ -67,58 +44,37 @@ public class EnemySight : MonoBehaviour
                 RaycastHit hit;
 
                 if(Physics.Raycast(transform.position + transform.up, direction.normalized, out hit, col.radius))
-                {
-                    
+                {                    
                     if(hit.collider.gameObject == player)
-                    {
                         playerInSight = true;
-                        
-
-                        //Animation shoot
-
-                        //playerHealth.TakeDamage(20);
-                    }
                 }
-            }
-
-            
+            }      
         }
     }
 
     void OnTriggerExit(Collider other)
     {
         if(other.gameObject == player)
-        {
             playerInSight = false;
-            
-        }
     }
 
     float CalculatePathLength(Vector3 targetPosition)
     {
         NavMeshPath path = nav.path;//new NavMeshPath();
 
-        //if(nav.enabled)
-        //{
         nav.CalculatePath(targetPosition, path);
-        //}
-        
 
         Vector3[] allWayPoints = new Vector3[path.corners.Length + 2];
         allWayPoints[0] = transform.position;
         allWayPoints[allWayPoints.Length - 1] = targetPosition;
 
         for(int i = 0; i < path.corners.Length; i++)
-        {
             allWayPoints[i + 1] = path.corners[i];
-        }
 
         float pathLength = 0;
 
         for(int i = 0; i < allWayPoints.Length - 1; i++)
-        {
             pathLength += Vector3.Distance(allWayPoints[i], allWayPoints[i + 1]);
-        }
 
         return pathLength;
     }
