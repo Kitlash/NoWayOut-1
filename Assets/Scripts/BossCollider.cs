d82876eef193af8d450ffa4840054e2ca5188f9b
Leramore <camille.louis@epita.fr>
2016-06-19 13:20:18 +0200

    boss

---
+++ Assets/Scripts/BossCollider.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BossCollider : MonoBehaviour
+{
+	[SerializeField]
+	float amount;
+
+	[SerializeField]
+	GameObject access;
+
+	[SerializeField]
+	GameObject wayout;
+
+	float BossLife;
+
+	void Start()
+	{
+		BossLife = GameObject.FindGameObjectWithTag ("boss1").GetComponent<EnemyLife>().Life;
+	}
+
+	void Update()
+	{
+		ColliderManagment ();
+
+		BossLife = GameObject.FindGameObjectWithTag ("boss1").GetComponent<EnemyLife>().Life;
+
+		if (BossLife <= 0) 
+		{
+			Destroy(gameObject);
+			Destroy(access);
+			Destroy(wayout);
+		}
+		Debug.Log (BossLife);
+
+	}
+
+	void OnTriggerEnter(Collider collider)
+	{
+		if (collider.gameObject.name == "Player") 
+		{
+			while (amount > 0) 
+			{
+				amount -= Time.deltaTime;
+				Debug.Log (amount);
+			}
+		}
+	}
+
+
+	void ColliderManagment()
+	{
+		if (amount <= 0) 
+		{
+			gameObject.GetComponent<Collider> ().isTrigger = false;
+			gameObject.GetComponent<Collider> ().enabled = true;
+
+			access.GetComponent<Collider> ().enabled = true;
+			access.GetComponent<Collider> ().isTrigger = false;
+		}
+	}
+}
---
+++ Assets/Scripts/LightSin.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightSin : MonoBehaviour 
+{
+	Light Cplight;
+
+//	float i = 0.2f;
+//	int factor = 1;
+
+	void Start () 
+	{
+		Cplight = GetComponent<Light> ();
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		Cplight = GetComponent<Light> ();
+		
+//		if (i > 1)
+//			factor = -1;
+//		if (i < -1)
+//			factor = 1;
+//
+//		i = factor * (i + Time.deltaTime * 0.2f);
+//
+//		Cplight.range += i
+
+		if (Cplight.range >= 4 && Cplight.range <= 5) 
+		{
+			Cplight.range += 1 * Time.deltaTime;
+		}
+
+		if (Cplight.range >= 5 && Cplight.range > 4) 
+		{
+			Cplight.range -= 1 * Time.deltaTime;
+		}
+	}
+}
