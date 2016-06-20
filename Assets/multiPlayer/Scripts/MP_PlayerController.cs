8a88a190bb0d546c31a4c973ddc4d8e31ff770ef
Remo <behanremoshan.benetruban@epita.fr>
2016-06-20 22:18:15 +0200

    FG

--- Assets/multiPlayer/Scripts/MP_PlayerController.cs
+++ Assets/multiPlayer/Scripts/MP_PlayerController.cs
@@ -7,9 +7,19 @@ public class MP_PlayerController : NetworkBehaviour
 {
     public GameObject bulletPrefab;
     public Transform bulletSpawn;
+    public Camera playerCamera;
+    public GameObject bulletSound;
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
 
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
 
-      // Update is called once per frame
+    void Start()
+    {
+
+    }
+    // Update is called once per frame
     void Update()
     {
         {
@@ -17,11 +27,16 @@ public class MP_PlayerController : NetworkBehaviour
             {
                 return;
             }
-            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+            var x = Input.GetAxis("Horizontal") * Time.deltaTime * 2.0f;
             var z = Input.GetAxis("Vertical") * Time.deltaTime * 2.0f;
 
-            transform.Rotate(0, x, 0);
-            transform.Translate(0, 0, z);
+
+            yaw += speedH * Input.GetAxis("Mouse X");
+            pitch -= speedV * Input.GetAxis("Mouse Y");
+
+            transform.eulerAngles = new Vector3(0, yaw, 0.0f);
+            playerCamera.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+            transform.Translate(x, 0, z);
 
             if (Input.GetKeyDown(KeyCode.Space))
             {
@@ -38,17 +53,23 @@ public class MP_PlayerController : NetworkBehaviour
     [Command]
     void CmdFire()
     {
+
         // Create the Bullet from the Bullet Prefab
         var bullet = (GameObject)Instantiate(bulletPrefab, bulletSpawn.position, bulletSpawn.rotation);
 
+        //Sound
+        var holdSound = (GameObject)Instantiate(bulletSound, bulletSpawn.position, bulletSpawn.rotation);
+
         // Add velocity to the bullet
-        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 22;
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 20;
 
         // Spawn the bullet on the Clients
         NetworkServer.Spawn(bullet);
+        NetworkServer.Spawn(holdSound);
 
         // Destroy the bullet after 2 seconds
         Destroy(bullet, 2.0f);
+        Destroy(holdSound, 2.0f);
 
     }
 }
