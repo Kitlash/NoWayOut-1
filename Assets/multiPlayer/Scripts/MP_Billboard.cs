e657be8f9fd8787286eb9f563e11f2e9540c6482
Remo <behanremoshan.benetruban@epita.fr>
2016-06-20 21:56:51 +0200

    Revert "Animating"
    This reverts commit 9d3dd3a828aaa2b6aa911665d15580144b314949.

--- Assets/multiPlayer/Scripts/MP_Billboard.cs
+++ Assets/multiPlayer/Scripts/MP_Billboard.cs
@@ -6,6 +6,6 @@ using System.Collections;
 public class MP_Billboard : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
-        transform.LookAt(Camera.current.transform);
+        transform.LookAt(Camera.main.gameObject.transform);
     }
 }
